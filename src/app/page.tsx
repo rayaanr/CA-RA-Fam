2a06ffe70d9c08a32cf57ddde71cbd3c412a63bb
rayaanr <rayaannsn@gmail.com>
2024-02-10 22:58:44 +0530

    create the tree sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@nextui-org/button";
+import dynamic from 'next/dynamic';
 import TopNav from "./components/TopNav";
+
+const TreeNoSSR = dynamic(() => import('./utils/Tree'), {
+    ssr: false
+});
 
 export default function Page() {
     return (
-        <div>
+        <main>
             <TopNav />
-            <Button>Click me</Button>
-        </div>
+            <TreeNoSSR />
+        </main>
     );
 }
